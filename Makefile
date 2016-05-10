4a47062b4c4bf95ac6122e7413b08405caed4262
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2016-05-10 18:45:28 +0900

    bumped version to 0.3.5.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.3.4
+VERSION=0.3.5
 TARGETS_NOVENDOR=$(shell glide novendor)
 
 all: bin/cachectl bin/cachectld
--- cachectl/const.go
+++ cachectl/const.go
@@ -1,7 +1,7 @@
 package cachectl
 
 const (
-	Version   = "0.3.4"
+	Version   = "0.3.5"
 	Cachectl  = "cachectl"
 	Cachectld = "cachectld"
 )
